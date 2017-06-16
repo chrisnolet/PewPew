cff6f27a7c648e42e57a5816944fc494844ba0e2
Chris Nolet <chris@relaunch.io>
2017-06-15 23:46:38 -0700

    Add initial laser position and rotation

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,10 +2,14 @@
 
 public class Laser : MonoBehaviour {
   public float speed = 10f;
+  public float lifetime = 1f;
 
   void Start() {
 
     // Set the initial velocity
-    GetComponent<Rigidbody>().velocity = new Vector3(speed, 0, 0);
+    GetComponent<Rigidbody>().velocity = new Vector3(0, speed, 0);
+
+    // Destroy after lifetime expires
+    Object.Destroy(gameObject, lifetime);
   }
 }
