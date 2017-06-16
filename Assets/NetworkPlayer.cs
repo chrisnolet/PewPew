d5fbd3104220e239643a7b8d3a2ea7d24468e0b0
Chris Nolet <chris@relaunch.io>
2017-06-15 22:38:10 -0700

    Add network commands and RPC calls

---
+++ Assets/NetworkPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour {
+  private GameObject laserPrefab;
+
+  void Start() {
+    
+  }
+
+  void Update() {
+
+    // Local player only
+    if (isLocalPlayer) {
+
+      // Input events
+      if (Input.GetButton("Fire1")) {
+        CmdFire();
+      }
+    }
+  }
+
+  [Command]
+  void CmdFire() {
+    NetworkServer.Spawn(laserPrefab);
+    RpcFire();
+  }
+
+  [ClientRpc]
+  void RpcFire() {
+
+  }
+}
