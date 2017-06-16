e3a633f472178f810bdf06008bf9ff8bc447ab6d
Chris Nolet <chris@relaunch.io>
2017-06-15 21:31:28 -0700

    Initial commit

---
+++ Assets/Editor/Scripts/ProjectSettings.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+
+public static class ProjectSettingsMenu {
+  [MenuItem("Settings/Input")]
+  static void Input() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Input");
+  }
+
+  [MenuItem("Settings/Tags and Layers")]
+  static void TagsAndLayers() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Tags and Layers");
+  }
+
+  [MenuItem("Settings/Audio")]
+  static void Audio() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Audio");
+  }
+
+  [MenuItem("Settings/Time")]
+  static void Time() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Time");
+  }
+
+  [MenuItem("Settings/Player")]
+  static void Player() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Player");
+  }
+
+  [MenuItem("Settings/Physics")]
+  static void Physics() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Physics");
+  }
+
+  [MenuItem("Settings/Physics 2D")]
+  static void Physics2D() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Physics 2D");
+  }
+
+  [MenuItem("Settings/Quality")]
+  static void Quality() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Quality");
+  }
+
+  [MenuItem("Settings/Graphics")]
+  static void Graphics() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Graphics");
+  }
+
+  [MenuItem("Settings/Network")]
+  static void Network() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Network");
+  }
+
+  [MenuItem("Settings/Editor")]
+  static void Editor() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Editor");
+  }
+
+  [MenuItem("Settings/Script Execution Order")]
+  static void ScriptExecutionOrder() {
+    EditorApplication.ExecuteMenuItem("Edit/Project Settings/Script Execution Order");
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+PewPew
+======
+
+Developed at SVVR HoloLens Hackathon (c) 2017
