8f55d5e83b0cf9c801cbc367eda6f4e26940fb45
Shiv Kumar <shivpkumar@gmail.com>
2017-06-16 23:45:00 -0700

    Add script to start server or client and set host IP

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Network : MonoBehaviour {
+    public bool isHost;
+    public string networkAddress = "192.168.43.174";
+
+	// Use this for initialization
+	void Start () {
+        if (isHost) {
+            NetworkManager.singleton.StartHost();
+        }   else {
+            NetworkManager.singleton.networkAddress = networkAddress;
+            NetworkManager.singleton.StartClient();
+        }
+	}
+}
