918fbdcd9b7fc204d897ff8ff44ead0e0b3b7ab2
Chris Nolet <chris@relaunch.io>
2017-06-17 01:10:25 -0700

    Automatically join when running on HoloLens

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.VR;
 
 public class Network : MonoBehaviour {
-    public bool isHost;
-    public string networkAddress = "192.168.43.174";
+  public string networkAddress = "192.168.43.174";
 
-	// Use this for initialization
-	void Start () {
-        if (isHost) {
-            NetworkManager.singleton.StartHost();
-        }   else {
-            NetworkManager.singleton.networkAddress = networkAddress;
-            NetworkManager.singleton.StartClient();
-        }
-	}
+  void Start () {
+
+    // Detect host device
+    if (VRDevice.isPresent) {
+
+      // Automatically join game when running on HoloLens
+      NetworkManager.singleton.networkAddress = networkAddress;
+      NetworkManager.singleton.StartClient();
+    }
+    else {
+
+      // Automatically host when running on desktop
+      NetworkManager.singleton.StartHost();
+    }
+  }
 }
